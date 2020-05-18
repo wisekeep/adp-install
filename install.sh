719fd2d6ccd5aff6204f3180f5e57d65b8e6ebe5
Marcelo Pires de Almeida <m4rc3lo.almeida@gmail.com>
2020-05-18 10:41:51 -0300

    [UPDATE] PHP4

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ add-apt-repository ppa:ondrej/php
 apt-get update
 apt-get upgrade -y
 
-echo "--- Instalando Apache ,PHP 7.3 e alguns modulos ---"
+echo "--- Instalando Apache ,PHP 7.4 e alguns modulos ---"
 apt-get install apache2
 
-apt-get install apt-transport-https curl composer git git-flow perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python php7.3 php7.3-cli php7.3-common php7.3-curl php7.3-dev php7.3-gd php-mbstring php7.3-mbstring php7.3-zip php7.3-xml php7.3-fpm libapache2-mod-php7.3 php-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-xml php7.3-xml php7.3-gd php7.3-zip php7.3-bcmath php-pgsql php7.3-sqlite3 zip unzip 
+apt-get install apt-transport-https curl composer git git-flow perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions
+apt-get install php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-dev php7.4-gd php-mbstring php7.4-mbstring php7.4-zip php7.4-xml php7.4-fpm libapache2-mod-php7.4 php-imagick php7.4-recode php7.4-tidy php7.4-xmlrpc php7.4-intl php-xml php7.4-xml php7.4-gd php7.4-zip php7.4-bcmath php-pgsql php7.4-sqlite3
+
+echo "--- Instalando Descompactadores ---"
+apt-get install zip unzip 
 
 echo "--- Adicionando repositorio do PostgreSQL---"
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
@@ -23,12 +27,11 @@ apt-get install postgresql pgadmin4
 echo "--- Instalando Banco SqLite3 ---" 
 apt-get install sqlite3 sqlitebrowser
 
-
 echo "--- Atualizando lista de pacotes ---"
 apt-get update
 
-echo "--- Habilitando o PHP 7.3 ---"
-a2dismod php5 | a2dismod 7.0 | a2dismod 7.1 | a2dismod 7.2 | a2enmod php7.3
+echo "--- Habilitando o PHP 7.4 ---"
+a2dismod php5 | a2dismod 7.0 | a2dismod 7.1 | a2dismod 7.2 | a2dismod 7.3 | a2enmod php7.4
 
 echo "--- Habilitando mod-rewrite do Apache ---"
 a2enmod rewrite
@@ -39,10 +42,10 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 echo "--- Instalando Banco NoSQL -> Redis <- ---" 
-apt-get install redis-server php7.3-redis
+apt-get install redis-server php7.4-redis
 
 echo "--- Instalando Banco SqLite3 ---" 
-apt-get install sqlite3 php7.3-sqlite3
+apt-get install sqlite3 
 
 # Instale apartir daqui o que você desejar 
 
